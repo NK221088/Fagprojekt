871d64dafb5dfc7c1865a82db3147ad83f126279
RallerR <51952733+RallerR@users.noreply.github.com>
2024-04-05 15:50:59 +0200

    Update ANN.py
    Add CNN model

--- ANN.py
+++ ANN.py
@@ -55,6 +55,23 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(len(np.unique(y)))
 ])
 
+model = models.Sequential()
+
+# Convolutional base
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(np.shape(X)[1], np.shape(X)[2], 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+
+# Dense layers on top
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))  # Dropout layer to reduce overfitting
+
+# Output layer
+model.add(layers.Dense(len(np.unique(y)), activation='softmax'))
+
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 import tensorflow as tf
