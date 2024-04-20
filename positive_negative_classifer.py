8b9403a0a03e9b07a322bdd3325da9748c7922a9
Isak <s224176@dtu.dk>
2024-04-20 10:38:44 +0200

    positive negative model updated

--- positive_negative_classifer.py
+++ positive_negative_classifer.py
@@ -1,25 +1,8 @@
 import numpy as np
-def Positive_Negativ_classifier(TappingTest, ControlTest, TappingTrain, ControlTrain, jointArray, labelIndx):
-    confusionMatrix = np.zeros((2,2)) # 0 = control, 1 = Tapping
-    #[[TP,FP]
-    # [FN,TN]]
+def Positive_Negativ_classifier(Xtrain, ytrain, Xtest, ytest):
     
+    predictions = (np.mean(Xtest, axis = (1,2)) > 0) == ytest
     
-    for val in np.concatenate((TappingTest,ControlTest), axis = 0): #Iterating over values belonging to training set (both tapping and control)
-            epochMean = np.mean(jointArray[val,:,:]) #Finding the mean of datapoint belonging to test set
-            
-            if epochMean > 0: #TRUE if squared distance is closer to meanTapping then meanControl
-                if val < labelIndx:                                                #Evaluating whether datapoint was labeled correctly or comitting type II error
-                    confusionMatrix[0,0] += 1 
-                else:
-                    confusionMatrix[0,1] += 1
-                    
-            elif epochMean < 0: #TRUE if squared distance is closer to meanControl then meanTapping
-                if val > labelIndx:                                                  #Evaluating whether datapoint was labeled correctly or comitting type I error
-                    confusionMatrix[1,1] += 1
-                else:
-                    confusionMatrix[1,0] += 1
-        
-    accuracy = (confusionMatrix[0,0] + confusionMatrix[1,1])/np.sum(confusionMatrix) #Calculating accuracy of current fold (TP + TN)/(N)
+    accuracy = sum(predictions) / len(predictions)
     
     return accuracy
