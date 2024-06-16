813691c47eb7791b0a79d1993a45dd82d02af8f5
Nikolai Treschow Kühl <s221088@dtu.dk>
2024-06-17 00:28:29 +0200

    First_two_level_cross_validation_on_Tapping_all_models_ready_for_HPC

--- ANN.py
+++ ANN.py
@@ -7,10 +7,10 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import TensorBoard
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from clr_callback import CyclicLR
 import gc
 import tensorflow as tf
 import os as os
+import shutil
 
 class fNirs_LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, initial_learning_rate, decay_steps, decay_rate):
@@ -34,106 +34,38 @@ class fNirs_LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
             "decay_rate": self.decay_rate,
         }
 
-def extract_features(X, model):
-    # Build the model by making a dummy prediction
-    dummy_data = tf.zeros((1, *X.shape[1:]))
-    model.predict(dummy_data)
-    
-    # Create the feature extractor model
-    feature_extractor = tf.keras.models.Model(inputs=model.input, outputs=model.layers[-2].output)
-    
-    # Extract features
-    features = feature_extractor.predict(X)
-    return features
-
 def ANN_classifier(Xtrain, ytrain, Xtest, ytest, theta):
     # Allow memory growth for the GPU
     physical_devices = tf.config.list_physical_devices('GPU')
     if physical_devices:
         try:
+            # Set memory growth to avoid allocating all GPU memory at once
             tf.config.experimental.set_memory_growth(physical_devices[0], True)
         except RuntimeError as e:
             print(e)
-
-    Xtest = (Xtest - np.mean(Xtrain, axis=0)) / np.std(Xtrain, axis=0)
-    Xtrain = (Xtrain - np.mean(Xtrain, axis=0)) / np.std(Xtrain, axis=0)
-
+    
+    Xtest = (Xtest - np.mean(Xtrain, axis = 0)) / np.std(Xtrain, axis = 0)
+    Xtrain = (Xtrain - np.mean(Xtrain, axis = 0)) / np.std(Xtrain, axis = 0)
+    
     X_train = tf.convert_to_tensor(Xtrain)
     y_train = tf.convert_to_tensor(ytrain)
     y_train = tf.cast(y_train, tf.int32)
     X_test = tf.convert_to_tensor(Xtest)
     y_test = tf.convert_to_tensor(ytest)
     y_test = tf.cast(y_test, tf.int32)
-
+    
     # Define your model
-    model = tf.keras.models.Sequential()
-
-    # Flatten input if necessary
-    if theta.get("layer_type") == "dense":
-        model.add(tf.keras.layers.Flatten(input_shape=(X_train.shape[1], X_train.shape[2])))
-
-    # Define activation function
-    def get_activation_function(name):
-        if name == "leakyrelu":
-            return tf.keras.layers.LeakyReLU()
-        elif name == "elu":
-            return tf.keras.layers.ELU()
-        else:
-            return tf.keras.activations.relu
-
-    activation_function = get_activation_function(theta.get("activation_function", "relu"))
-
-    # Retrieve dropout rate or set default
-    dropout_rate = theta.get("dropout_rate", 0.2)
-    initial_dropout_rate = dropout_rate * 0.5  # Scaling down for initial layers
-
-    # Add initial layers based on the type specified in theta
-    if theta.get("layer_type") == "conv1d":
-        model.add(tf.keras.layers.Conv1D(filters=theta["neuron1"], kernel_size=3, activation=activation_function, input_shape=(X_train.shape[1], X_train.shape[2])))
-        model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
-        model.add(tf.keras.layers.Dropout(initial_dropout_rate))
-    elif theta.get("layer_type") in ["lstm", "gru"]:
-        recurrent_layer = tf.keras.layers.LSTM if theta["layer_type"] == "lstm" else tf.keras.layers.GRU
-        model.add(recurrent_layer(theta["neuron1"], activation=activation_function, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-        model.add(tf.keras.layers.Dropout(initial_dropout_rate))
-    else:
-        model.add(tf.keras.layers.Dense(theta["neuron1"], activation=activation_function))
-        model.add(tf.keras.layers.Dropout(initial_dropout_rate))
-
-    # Add additional layers based on theta["layers"]
-    if theta["layers"] == 6:
-        if theta.get("layer_type") == "conv1d":
-            model.add(tf.keras.layers.Conv1D(filters=theta["neuron2"], kernel_size=3, activation=activation_function))
-            model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
-        elif theta.get("layer_type") in ["lstm", "gru"]:
-            model.add(recurrent_layer(theta["neuron2"], activation=activation_function, return_sequences=True))
-        else:
-            model.add(tf.keras.layers.Dense(theta["neuron2"], activation=activation_function))
-        model.add(tf.keras.layers.Dropout(dropout_rate))
-    elif theta["layers"] == 8:
-        if theta.get("layer_type") == "conv1d":
-            model.add(tf.keras.layers.Conv1D(filters=theta["neuron2"], kernel_size=3, activation=activation_function))
-            model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
-            model.add(tf.keras.layers.Conv1D(filters=theta["neuron1"], kernel_size=3, activation=activation_function))
-            model.add(tf.keras.layers.MaxPooling1D(pool_size=2))
-        elif theta.get("layer_type") in ["lstm", "gru"]:
-            model.add(recurrent_layer(theta["neuron2"], activation=activation_function, return_sequences=True))
-            model.add(recurrent_layer(theta["neuron1"], activation=activation_function, return_sequences=True))
-        else:
-            model.add(tf.keras.layers.Dense(theta["neuron2"], activation=activation_function))
-            model.add(tf.keras.layers.Dropout(dropout_rate))
-            model.add(tf.keras.layers.Dense(theta["neuron1"], activation=activation_function))
-        model.add(tf.keras.layers.Dropout(dropout_rate))
-
-    # Add the output layer for binary classification
-    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-    # Compile the model
+    model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(np.shape(X_train)[1], np.shape(X_train)[2])),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, activation='sigmoid')  # Output layer for binary classification. The units is 1, as the output of the sigmoid function represents the probability of belonging to the positive class
+    ])
+    
     loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=False)
-    epochs = 300
+    epochs = 100
     batch_size = 100
     log_dir = "logs/"
-    
     # Clear old logs
     for file in os.listdir(log_dir):
         file_path = os.path.join(log_dir, file)
@@ -145,49 +77,25 @@ def ANN_classifier(Xtrain, ytrain, Xtest, ytest, theta):
         except Exception as e:
             print(f'Failed to delete {file_path}. Reason: {e}')
     tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    if theta["learning_rate"] == "decrease":
-        initial_learning_rate = 0.001
-        decay_steps = 50
-        decay_rate = 0.9
-
-        optimizer = tf.keras.optimizers.Adam(
-            learning_rate=fNirs_LRSchedule(
-                initial_learning_rate=initial_learning_rate,
-                decay_steps=decay_steps,
-                decay_rate=decay_rate,
-            )
-        )
-        model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
-        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[tensorboard_callback], verbose=0)
-    elif theta["learning_rate"] == "clr":
-        initial_learning_rate = 0.00025
-        max_learning_rate = 0.001
-        step_size = 20
-
-        optimizer = tf.keras.optimizers.Adam()
-        model.compile(optimizer=optimizer, loss=loss_fn, metrics=['accuracy'])
-        clr = CyclicLR(base_lr=initial_learning_rate, max_lr=max_learning_rate, step_size=step_size, mode='exp_range')
-        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[tensorboard_callback, clr], verbose=0)
-
-    # Ensure the model is built
-    dummy_data = tf.zeros((1, X_train.shape[1], X_train.shape[2]))
-    model(dummy_data)  # This will build the model
-
-    # Create the feature extractor model
-    feature_extractor = tf.keras.models.Model(inputs=model.input, outputs=model.layers[-2].output)
     
-    # Extract features from the trained network
-    train_features = feature_extractor.predict(X_train)
-    test_features = feature_extractor.predict(X_test)
-
-    # Train an SVM on the extracted features
-    svm = SVC(kernel='rbf')
-    svm.fit(train_features, ytrain)
-
-    # Evaluate the SVM
-    y_pred = svm.predict(test_features)
-    accuracy = accuracy_score(ytest, y_pred)
+    initial_learning_rate = 0.01
+    decay_steps = tf.constant(50, dtype=tf.int64)
+    decay_rate = 0.9
+    
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=fNirs_LRSchedule(
+            initial_learning_rate = initial_learning_rate,
+            decay_steps = decay_steps,
+            decay_rate = decay_rate,
+        )
+    )
+    model.compile(optimizer=optimizer,
+                loss=loss_fn, 
+                metrics=['accuracy'])
+    
+    model.fit(X_train, y_train, epochs = epochs, batch_size = batch_size, callbacks=[tensorboard_callback],verbose = 0)
+    
+    loss, accuracy = model.evaluate(X_test,  y_test, verbose=0)
 
     # Clear session and delete model to free up memory
     tf.keras.backend.clear_session()
--- two_level_cv_test.py
+++ two_level_cv_test.py
@@ -17,8 +17,8 @@ import pandas as pd
 ############################
 
 # Data set:
-data_set = "fNIRS_Alexandros_Healthy_data"
-epoch_type = "Imagery"
+data_set = "fNirs_motor_full_data"
+epoch_type = "Tapping"
 combine_strategy = "mean"
 individuals = True # CANNOT BE CHANGED IN THIS SCRIPT
 if not individuals:
@@ -38,9 +38,12 @@ interpolate_bad_channels = False
 save_results = True
 
 # Models
-# ANN = model(name = "ANN")
+SVM = model(name = "SVM")
+ANN = model(name = "ANN")
+Mean = model(name = "Mean")
+Baseline = model(name = "Baseline")
+PosNeg = model(name = "PosNeg")
 CNN = model(name = "CNN")
-# SVM = model(name = "SVM")
 
 # ANN.theta = {
 #     "neuron1": [60, 128],
@@ -52,16 +55,20 @@ CNN = model(name = "CNN")
 #     "dropout_rate": [0.3]
 # }
 
-# ANN.theta = {
-#     "neuron1": [60, 128],
-#     "neuron2": [100, 150, 300],
-#     "layers": [6, 8],
-#     "learning_rate": ["decrease", "clr"],
-# }
-CNN.theta = {"base_learning_rate": [0.001, 0.01, 0.1], "number_of_layers": [50, 75, 100, 125], "batch_size": [32, 64, 128]}
+ANN.theta = {
+    "neuron1": [60, 128],
+    "neuron2": [100, 150, 300],
+    "layers": [6, 8],
+    "learning_rate": ["decrease", "clr"],
+}
+CNN.theta = {"base_learning_rate": [0.001, 0.1], "number_of_layers": [50, 75, 100], "batch_size": [32]}
+SVM.theta = {"kernel": ["rbf"], "C": [1.0], "gamma": ['scale'], "degree": [3], "coef0": [0.0]}
+Mean.theta = {}
+Baseline.theta = {}
+PosNeg.theta = {}
 
 ANN_AND_SVM = False
-modelList = [CNN]
+modelList = [SVM, ANN, Mean, Baseline, PosNeg, CNN]
 
 all_epochs, data_name, all_data, freq, data_types, all_individuals = load_data(data_set = data_set, short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels)
 accuracy, E_genList, E_test = two_level_cross_validation(modelList = modelList, K2 = K2, startTime = startTime, stopTime = stopTime, freq=freq, dataset = all_individuals, data_types=data_types)
