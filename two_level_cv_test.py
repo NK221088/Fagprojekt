41015aef10ee846039982a50b8dc4e6bc2991af8
Nikolai Treschow Kühl <s221088@dtu.dk>
2024-06-16 14:52:55 +0200

    Made ANNs ready for HPC

--- two_level_cv_test.py
+++ two_level_cv_test.py
@@ -37,19 +37,26 @@ interpolate_bad_channels = False
 save_results = True
 
 # Models
-# ANN = model(name = "ANN")
+ANN = model(name = "ANN")
 # CNN = model(name = "CNN")
-SVM = model(name = "SVM")
+# SVM = model(name = "SVM")
+
+# ANN.theta = {
+#     "neuron1": [60, 128],
+#     "neuron2": [100, 150, 300],
+#     "layers": [6, 8],
+#     "learning_rate": ["decrease", "clr"],
+#     "layer_type": ["dense", "conv1d", "lstm", "gru"],
+#     "activation_function": ["relu", "leakyrelu", "elu"],
+#     "dropout_rate": [0.3]
+# }
 
 ANN.theta = {
     "neuron1": [60, 128],
     "neuron2": [100, 150, 300],
     "layers": [6, 8],
-    "learning_rate": ["decrease", "clr"],
-    "layer_type": ["dense", "conv1d", "lstm", "gru"],
-    "activation_function": ["relu", "leakyrelu", "elu"],
-    "dropout_rate": [0.3]
 }
+
 ANN_AND_SVM = True
 modelList = [ANN]
 
