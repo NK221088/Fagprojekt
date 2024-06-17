5fe405d6601e5f1137734cd41cd042d1cc3a9dd2
Nikolai Treschow Kühl <s221088@dtu.dk>
2024-06-17 09:40:51 +0200

    Made complex ANN ready for HPC

--- two_level_cv_test.py
+++ two_level_cv_test.py
@@ -17,8 +17,8 @@ import pandas as pd
 ############################
 
 # Data set:
-data_set = "fNirs_motor_full_data"
-epoch_type = "Tapping"
+data_set = "fNIRS_Alexandros_Healthy_data" #"fNirs_motor_full_data"
+epoch_type = "Imagery"
 combine_strategy = "mean"
 individuals = True # CANNOT BE CHANGED IN THIS SCRIPT
 if not individuals:
@@ -38,12 +38,12 @@ interpolate_bad_channels = False
 save_results = True
 
 # Models
-SVM = model(name = "SVM")
+# SVM = model(name = "SVM")
 ANN = model(name = "ANN")
-Mean = model(name = "Mean")
-Baseline = model(name = "Baseline")
-PosNeg = model(name = "PosNeg")
-CNN = model(name = "CNN")
+# Mean = model(name = "Mean")
+# Baseline = model(name = "Baseline")
+# PosNeg = model(name = "PosNeg")
+# CNN = model(name = "CNN")
 
 # ANN.theta = {
 #     "neuron1": [60, 128],
@@ -61,14 +61,15 @@ ANN.theta = {
     "layers": [6, 8],
     "learning_rate": ["decrease", "clr"],
 }
-CNN.theta = {"base_learning_rate": [0.001, 0.1], "number_of_layers": [50, 75, 100], "batch_size": [32]}
-SVM.theta = {"kernel": ["rbf", "linear"], "C": [1.0, 10], "gamma": ['scale', 'auto'], "degree": [3, 5], "coef0": [0.0, 0.5]}
-Mean.theta = {}
-Baseline.theta = {}
-PosNeg.theta = {}
+# CNN.theta = {"base_learning_rate": [0.001, 0.1], "number_of_layers": [50, 75, 100], "batch_size": [32]}
+# SVM.theta = {"kernel": ["rbf", "linear"], "C": [1.0, 10], "gamma": ['scale', 'auto'], "degree": [3, 5], "coef0": [0.0, 0.5]}
+# Mean.theta = {}
+# Baseline.theta = {}
+# PosNeg.theta = {}
 
 ANN_AND_SVM = False
-modelList = [SVM, ANN, Baseline, PosNeg, CNN]
+# modelList = [SVM, ANN, Baseline, PosNeg, CNN]
+modelList = [ANN]
 
 all_epochs, data_name, all_data, freq, data_types, all_individuals = load_data(data_set = data_set, short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels)
 accuracy, E_genList, E_test = two_level_cross_validation(modelList = modelList, K2 = K2, startTime = startTime, stopTime = stopTime, freq=freq, dataset = all_individuals, data_types=data_types)
