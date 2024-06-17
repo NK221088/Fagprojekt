020ea2b95748b7b3734c7e775736c85f373734ad
Nikolai Treschow Kühl <s221088@dtu.dk>
2024-06-17 12:36:21 +0200

    Made ANN ready for HPC

--- two_level_cv_test.py
+++ two_level_cv_test.py
@@ -39,21 +39,21 @@ save_results = True
 
 # Models
 # SVM = model(name = "SVM")
-# ANN = model(name = "ANN")
+ANN = model(name = "ANN")
 # Mean = model(name = "Mean")
 # Baseline = model(name = "Baseline")
 # PosNeg = model(name = "PosNeg")
-CNN = model(name = "CNN")
+# CNN = model(name = "CNN")
 
-# ANN.theta = {
-#     "neuron1": [60, 128],
-#     "neuron2": [100, 150, 300],
-#     "layers": [6, 8],
-#     "learning_rate": ["decrease", "clr"],
-#     "layer_type": ["dense", "conv1d", "lstm", "gru"],
-#     "activation_function": ["relu", "leakyrelu", "elu"],
-#     "dropout_rate": [0.3]
-# }
+ANN.theta = {
+    "neuron1": [60, 128],
+    "neuron2": [100, 150, 300],
+    "layers": [6, 8],
+    "learning_rate": ["decrease", "clr"],
+    "layer_type": ["dense", "conv1d", "lstm", "gru"],
+    "activation_function": ["relu", "leakyrelu", "elu"],
+    "dropout_rate": [0.3]
+}
 
 # ANN.theta = {
 #     "neuron1": [60, 128],
@@ -67,12 +67,12 @@ CNN = model(name = "CNN")
 # Baseline.theta = {}
 # PosNeg.theta = {}
 
-CNN.theta = {"base_learning_rate": [0.001, 0.01, 0.1], "number_of_layers": [50, 75, 100, 125], "batch_size": [32, 64, 128]}
+# CNN.theta = {"base_learning_rate": [0.001, 0.01, 0.1], "number_of_layers": [50, 75, 100, 125], "batch_size": [32, 64, 128]}
 
 
 # ANN_AND_SVM = False
 # modelList = [SVM, ANN, Baseline, PosNeg, CNN]
-modelList = [CNN]
+modelList = [ANN]
 
 all_epochs, data_name, all_data, freq, data_types, all_individuals = load_data(data_set = data_set, short_channel_correction = short_channel_correction, negative_correlation_enhancement = negative_correlation_enhancement, individuals = individuals, interpolate_bad_channels=interpolate_bad_channels)
 accuracy, E_genList, E_test = two_level_cross_validation(modelList = modelList, K2 = K2, startTime = startTime, stopTime = stopTime, freq=freq, dataset = all_individuals, data_types=data_types)
