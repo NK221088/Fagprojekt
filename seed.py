6f1b8e0d3f54accf7300fc9567e66ff47022734d
JWHansen96 <jwhansen1996@gmail.com>
2024-06-20 10:32:17 +0200

    seed = 82
    seed = 82

--- seed.py
+++ seed.py
@@ -4,7 +4,7 @@ import random
 import os
 
 # Set seeds for reproducibility
-def set_seeds(seed=72):
+def set_seeds(seed=82):
     tf.random.set_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
