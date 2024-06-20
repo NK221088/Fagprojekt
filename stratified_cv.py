872e5c25cb2733c2de1518d421d5caee3b3580d1
Nikolai Treschow Kühl <s221088@dtu.dk>
2024-06-20 02:36:35 +0200

    Implemented random seed 42

--- stratified_cv.py
+++ stratified_cv.py
@@ -8,6 +8,23 @@ from model import model
 from tqdm import tqdm
 import itertools
 
+import tensorflow as tf
+import numpy as np
+import random
+import os
+
+# Set seeds for reproducibility
+def set_seeds(seed=42):
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    # If using TensorFlow with GPU:
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+set_seeds()
+
 
 def StratifiedCV(modelList, tappingArray, controlArray, startTime, stopTime, iter_n=0, K = 4, freq = 7.81):
     
